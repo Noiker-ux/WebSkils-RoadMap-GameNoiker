bbac36cec19aab3334be9f3a79101ea53bdfeaf0
Noiker <71761947+Noiker-ux@users.noreply.github.com>
2023-06-20 23:30:15 +0200

    Add files via upload

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,17 @@
 'use strict';
+const RoadMapGaming = 'NoikerRoadMapThemes.js'
 window.addEventListener('DOMContentLoaded',()=>{
-    document.querySelector('.theme-toggler__switch').addEventListener('click',(e)=>{
+    const theme = localStorage.getItem(RoadMapGaming)
+    if (theme=='true'){
+        document.body.classList.add("dark-mode")
+    }
+
+    document.querySelector('.theme-toggler__switch').addEventListener('click',()=>{
         document.body.classList.toggle("dark-mode")
-    })
+        if (document.body.classList.contains('dark-mode')){
+            localStorage.setItem(RoadMapGaming,false);
+        } else {
+            localStorage.setItem(RoadMapGaming,true);
+        }
+    });
 });
