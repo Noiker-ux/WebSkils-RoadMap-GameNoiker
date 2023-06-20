f3e0dad89d3ece4ca40b4777c5a7a3d2bf3e2cb5
Alex Vyugin <pasha_pasha_201@mail.ru>
2023-06-20 17:59:22 +0200

    layout

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,6 @@
 'use strict';
 window.addEventListener('DOMContentLoaded',()=>{
     document.querySelector('.theme-toggler__switch').addEventListener('click',(e)=>{
-        e.stopPropagation();
-        console.log('click');
         document.body.classList.toggle("dark-mode")
     })
 });
