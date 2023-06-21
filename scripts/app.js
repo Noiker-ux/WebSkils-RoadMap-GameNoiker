36e567e40d4942a9093b559d4e41cdf8e4fd7f42
Alex Vyugin <pasha_pasha_201@mail.ru>
2023-06-21 17:56:21 +0200

    fancybox upLvl

--- scripts/app.js
+++ scripts/app.js
@@ -3,34 +3,35 @@
 
 window.addEventListener('DOMContentLoaded',()=>{
 // Переменная LS для темы
-const RoadMapGamingTheme = 'NoikerRoadMapThemes.js'
-const str=`
-<div class="faq" style="display: none; width: 500px;" id="FAQhidden">
-    <h3 class="faq__title">Web Skills FAQ</h3>
-    <div class="faq__qestion">
-        <b class="faq__subtitle">What is Web Skills?</b>
-        <p class="faq__answer">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate aliquam laudantium perferendis eos quo? Harum repudiandae dolor ipsam eligendi? Blanditiis neque ab dolores distinctio delectus aliquam sed ut numquam fugit?
-        Mollitia ipsa facere quibusdam, tempora repudiandae optio consequuntur tenetur distinctio iure tempore suscipit ad commodi nisi!</p>
-    </div>
-    <div class="faq__qestion">
-        <b class="faq__subtitle">What is Web Skills?</b>
-        <p class="faq__answer">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate aliquam laudantium perferendis eos quo? Harum repudiandae dolor ipsam eligendi? Blanditiis neque ab dolores distinctio delectus aliquam sed ut numquam fugit?
-        Mollitia ipsa facere quibusdam, tempora repudiandae optio consequuntur tenetur distinctio iure tempore suscipit ad commodi nisi!</p>
-    </div>
-    <div class="faq__qestion">
-        <b class="faq__subtitle">What is Web Skills?</b>
-        <p class="faq__answer">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate aliquam laudantium perferendis eos quo? Harum repudiandae dolor ipsam eligendi? Blanditiis neque ab dolores distinctio delectus aliquam sed ut numquam fugit?
-        Mollitia ipsa facere quibusdam, tempora repudiandae optio consequuntur tenetur distinctio iure tempore suscipit ad commodi nisi!</p>
-    </div>
-</div>`;
+    const RoadMapGamingTheme = 'NoikerRoadMapThemes.js'
+    
+    const str=`
+    <div class="faq" style="display: none; width: 500px;" id="FAQhidden">
+        <h3 class="faq__title">Web Skills FAQ</h3>
+        <div class="faq__qestion">
+            <b class="faq__subtitle">What is Web Skills?</b>
+            <p class="faq__answer">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate aliquam laudantium perferendis eos quo? Harum repudiandae dolor ipsam eligendi? Blanditiis neque ab dolores distinctio delectus aliquam sed ut numquam fugit?
+            Mollitia ipsa facere quibusdam, tempora repudiandae optio consequuntur tenetur distinctio iure tempore suscipit ad commodi nisi!</p>
+        </div>
+        <div class="faq__qestion">
+            <b class="faq__subtitle">What is Web Skills?</b>
+            <p class="faq__answer">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate aliquam laudantium perferendis eos quo? Harum repudiandae dolor ipsam eligendi? Blanditiis neque ab dolores distinctio delectus aliquam sed ut numquam fugit?
+            Mollitia ipsa facere quibusdam, tempora repudiandae optio consequuntur tenetur distinctio iure tempore suscipit ad commodi nisi!</p>
+        </div>
+        <div class="faq__qestion">
+            <b class="faq__subtitle">What is Web Skills?</b>
+            <p class="faq__answer">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate aliquam laudantium perferendis eos quo? Harum repudiandae dolor ipsam eligendi? Blanditiis neque ab dolores distinctio delectus aliquam sed ut numquam fugit?
+            Mollitia ipsa facere quibusdam, tempora repudiandae optio consequuntur tenetur distinctio iure tempore suscipit ad commodi nisi!</p>
+        </div>
+    </div>`;
 
     document.getElementById('faq__btn-id').addEventListener('click',()=>{
-        document.querySelector('.main').insertAdjacentHTML('beforeend',str);
             $.fancybox.open({
-                src: '#FAQhidden',
-                type: 'inline'
+                src: str,
+                type: 'inline',
             });
     })
+
 
     // переключение темы светлая темная
     document.querySelector('.theme-toggler__switch').addEventListener('click',()=>{
