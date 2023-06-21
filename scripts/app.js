692dfbabfaa6d800ab1f90fdd59e1e4f8c6dc379
Alex Vyugin <pasha_pasha_201@mail.ru>
2023-06-21 12:12:02 +0200

    layout for first try

--- scripts/app.js
+++ scripts/app.js
@@ -1,17 +1,41 @@
 'use strict';
-const RoadMapGaming = 'NoikerRoadMapThemes.js'
-window.addEventListener('DOMContentLoaded',()=>{
-    const theme = localStorage.getItem(RoadMapGaming)
-    if (theme=='true'){
-        document.body.classList.add("dark-mode")
-    }
 
+
+window.addEventListener('DOMContentLoaded',()=>{
+// Переменная LS для темы
+const RoadMapGamingTheme = 'NoikerRoadMapThemes.js'
+const str=`
+<div class="faq" style="display: none; width: 500px;" id="FAQhidden">
+    <h3 class="faq__title">Web Skills FAQ</h3>
+    <div class="faq__qestion">
+        <b class="faq__subtitle">What is Web Skills?</b>
+        <p class="faq__answer">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate aliquam laudantium perferendis eos quo? Harum repudiandae dolor ipsam eligendi? Blanditiis neque ab dolores distinctio delectus aliquam sed ut numquam fugit?
+        Mollitia ipsa facere quibusdam, tempora repudiandae optio consequuntur tenetur distinctio iure tempore suscipit ad commodi nisi!</p>
+    </div>
+    <div class="faq__qestion">
+        <b class="faq__subtitle">What is Web Skills?</b>
+        <p class="faq__answer">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate aliquam laudantium perferendis eos quo? Harum repudiandae dolor ipsam eligendi? Blanditiis neque ab dolores distinctio delectus aliquam sed ut numquam fugit?
+        Mollitia ipsa facere quibusdam, tempora repudiandae optio consequuntur tenetur distinctio iure tempore suscipit ad commodi nisi!</p>
+    </div>
+    <div class="faq__qestion">
+        <b class="faq__subtitle">What is Web Skills?</b>
+        <p class="faq__answer">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate aliquam laudantium perferendis eos quo? Harum repudiandae dolor ipsam eligendi? Blanditiis neque ab dolores distinctio delectus aliquam sed ut numquam fugit?
+        Mollitia ipsa facere quibusdam, tempora repudiandae optio consequuntur tenetur distinctio iure tempore suscipit ad commodi nisi!</p>
+    </div>
+</div>`;
+
+    document.getElementById('faq__btn-id').addEventListener('click',()=>{
+        document.querySelector('.main').insertAdjacentHTML('beforeend',str);
+            $.fancybox.open({
+                src: '#FAQhidden',
+                type: 'inline'
+            });
+    })
+
+    // переключение темы светлая темная
     document.querySelector('.theme-toggler__switch').addEventListener('click',()=>{
         document.body.classList.toggle("dark-mode")
-        if (document.body.classList.contains('dark-mode')){
-            localStorage.setItem(RoadMapGaming,false);
-        } else {
-            localStorage.setItem(RoadMapGaming,true);
-        }
     });
+
+
 });
